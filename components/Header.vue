5fa6f3a326b3668433abe9cd2ac2e8dd5ba5952f
salmatsaniatul <141211517+salmatsaniatul@users.noreply.github.com>
2024-02-26 16:05:21 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+      <img src="~/assets/img/LOGO-SMK4.PNG" alt="logo" />
+    </div>
+
+    <div class="col-lg-11">
+      <h1>PERPUS DIGITAL</h1>
+      <address>
+        <div>SMKN 4 Tasikmalaya</div>
+        Jl.Depok,Sukamenak,Purbaratu
+      </address>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bg-primary {
+  background-color: #1a41a5 !important;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container-fluid">
+    <div class="row my-5">
+      <div class="col-lg-6">
+        <nuxt-link to="/pengunjung/tambah">
+          <div class="card bg-pengunjung rounded-5">
+            <div class="card-body">
+              <h2>Pengunjung</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+
+      <div class="col-lg-6">
+        <nuxt-link to="/buku">
+          <div class="card bg-buku rounded-5">
+            <div class="card-body">
+              <h2>Cari Buku</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  height: 250px;
+  box-shadow: 1px 1px 10px #1a41a5;
+}
+.card.bg-pengunjung {
+  background-image: url("../assets/img/bg-kunjungan.jpeg");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  color: black;
+}
+.card.bg-buku {
+  background: url("../assets/img/bg-cari buku.jpeg") no-repeat center center;
+  background-size: cover;
+  color: black;
+}
+</style>
