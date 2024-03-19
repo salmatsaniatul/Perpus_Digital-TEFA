c6d4c455c5f0215a2450c5a5dbd1b98fd8d03ebd
salmatsaniatul <141211517+salmatsaniatul@users.noreply.github.com>
2024-03-19 21:58:09 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,62 @@
 <template>
-  <div class="row p-3 bg-primary text-white">
-    <div class="col-lg-1">
-      <img src="~/assets/img/LOGO-SMK4.PNG" alt="logo" />
+  <div class="Header">
+    <div class="h-img">
+      <img src="~/assets/img/LOGO-SMK4.png" />
     </div>
-
-    <div class="col-lg-11">
-      <h1>PERPUS DIGITAL</h1>
-      <address>
-        <div>SMKN 4 Tasikmalaya</div>
-        Jl.Depok,Sukamenak,Purbaratu
-      </address>
+    <div class="h-text">
+      <div class="t-judul">
+        <p>PERPUS DIGITAL</p>
+        <p>SMK NEGERI 4 TASIKMALAYA</p>
+      </div>
+      <p>Jl.Depok, Sukamenak, Purbaratu</p>
     </div>
   </div>
 </template>
 
 <style scoped>
-.bg-primary {
+.Header {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 150px;
   background-color: #1a41a5 !important;
+  color: rgb(255, 255, 255, 100%);
+}
+
+.h-img {
+  margin-left: 25px;
+}
+
+.h-img > img {
+  width: 80%;
+}
+
+.h-text {
+  margin-left: -15px;
+  line-height: 10px;
+}
+
+.t-judul {
+  font-size: 20px;
+  font-weight: 500;
+}
+@media only screen and (max-width: 40em) {
+  .h-img {
+    margin-left: 0.8rem;
+  }
+
+  .h-img > img {
+    width: 70%;
+  }
+
+  .h-text {
+    margin-left: -1.5rem;
+    line-height: 5px;
+  }
+
+  .t-judul {
+    font-size: 15px;
+    font-weight: 500;
+  }
 }
 </style>
